1fe7c93468df6ec635d55852de4e7a8fe441b66d
Nam Nguyen <nam@namnm.com>
2024-12-19 20:54:55 +0700

    Fix css: nib reset, auto prefixer

--- app/src/urlFactory.js
+++ app/src/urlFactory.js
@@ -4,6 +4,7 @@ if (window.location.hostname === 'test.mediasoup.org') protooPort = 4444
 
 export function getProtooUrl({ roomId, peerId, consumerReplicas }) {
   const hostname = window.location.hostname
+  // const hostname = 'v3demo.mediasoup.org'
 
   return `wss://${hostname}:${protooPort}/?roomId=${roomId}&peerId=${peerId}&consumerReplicas=${consumerReplicas}`
 }
